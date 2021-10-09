4b1de8f0258dc3f718a71520d08709a1ed4babc8
Toomas Tamm <toomas@toomastamm.ee>
2021-10-09 11:30:01 +0000

    [Req 1 - Pass] The customers can file an insurance claim.

--- src/main/java/AIUSStarter.java
+++ src/main/java/AIUSStarter.java
@@ -1,6 +1,56 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class AIUSStarter {
 
+    public static List<InsuranceClaim> claims;
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        init();
+        Scanner s = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("-----------");
+            System.out.println("Welcome to AIUS.");
+            System.out.println("-----------");
+            System.out.println("1. Exit");
+            System.out.println("2. File insurance claim");
+            System.out.println("3. ");
+            System.out.println("4. ");
+            System.out.println("-----------");
+            System.out.println("Choose action: ");
+
+            String input = s.nextLine();
+
+            switch (input) {
+                case "1" -> System.exit(0);
+                case "2" -> fileInsuranceClaim(s);
+                case "3" -> throw new UnsupportedOperationException();
+                case "4" -> throw new UnsupportedOperationException();
+                default -> {
+                }
+            }
+        }
+    }
+
+    public static void init() {
+        claims = new ArrayList<>();
+    }
+
+
+    public static void fileInsuranceClaim(Scanner s) {
+        System.out.println("-----------");
+        System.out.println("Insurance claim form.");
+        System.out.println("Name of claimant:");
+        String name = s.nextLine();
+        System.out.println("What happened:");
+        String description = s.nextLine();
+        System.out.println("Claim sum:");
+        float sum = Float.parseFloat(s.nextLine());
+
+        claims.add(new InsuranceClaim(name, description, sum, false));
+        System.out.println("Your claim has been filed, returning to menu.");
     }
 }
+
---
+++ src/main/java/InsuranceClaim.java
@@ -0,0 +1,45 @@
+public class InsuranceClaim {
+    private String claimerName;
+    private String description;
+    private float claimAmount;
+    private boolean approved;
+
+    public InsuranceClaim(String claimerName, String description, float claimAmount, boolean approved) {
+        this.claimerName = claimerName;
+        this.description = description;
+        this.claimAmount = claimAmount;
+        this.approved = approved;
+    }
+
+    public String getClaimerName() {
+        return claimerName;
+    }
+
+    public void setClaimerName(String claimerName) {
+        this.claimerName = claimerName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getClaimAmount() {
+        return claimAmount;
+    }
+
+    public void setClaimAmount(float claimAmount) {
+        this.claimAmount = claimAmount;
+    }
+
+    public boolean isApproved() {
+        return approved;
+    }
+
+    public void setApproved(boolean approved) {
+        this.approved = approved;
+    }
+}
