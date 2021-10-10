23b612b45f47a95f58cda873ac3dbadf720ae90e
Kaarel Ryysak <kaarel@ryysak.com>
2021-10-10 10:12:11 +0300

    [Req 3 - Fail] The underwriters can request data from the system

---
+++ src/test/java/DataRequestTest.java
@@ -0,0 +1,74 @@
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import java.util.Scanner;
+
+public class DataRequestTest {
+    private final java.io.ByteArrayOutputStream outContent = new java.io.ByteArrayOutputStream();
+    private final java.io.ByteArrayOutputStream errContent = new java.io.ByteArrayOutputStream();
+    private final java.io.PrintStream originalOut = System.out;
+    private final java.io.PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    private void setupTestEnv()
+    {
+        AIUSStarter.init();
+
+        String name = "TestingName";
+        String description = "TestingDescription";
+        String amount = "9999.99";
+
+        Scanner s = new Scanner(String.format("%s\n%s\n%s\n", name, description, amount)).useDelimiter("\n");
+        AIUSStarter.fileInsuranceClaim(s);
+    }
+
+    @Test
+    public void testInsuranceDataRequest() {
+        setupTestEnv();
+        String name = "SecondTestEntry";
+        String description = "SecondDescription";
+        String amount = "55.0";
+
+        Scanner s = new Scanner(String.format("%s\n%s\n%s\n", name, description, amount)).useDelimiter("\n");
+        AIUSStarter.fileInsuranceClaim(s);
+
+        AIUSStarter.requestInsuranceClaimsData();
+        Assertions.assertEquals(
+                "-----------\r\n" +
+                        "ID: 0\r\n" +
+                        "Name: TestingName\r\n" +
+                        "Description: TestingDescription\r\n" +
+                        "Amount: 9999.99\r\n" +
+                        "-----------\r\n" +
+                        "ID: 1\r\n" +
+                        "Name: SecondTestEntry\r\n" +
+                        "Description: SecondDescription\r\n" +
+                        "Amount: 55.0\r\n" +
+                        "-----------\r\n" +
+                        "Returning to menu.\r\n", outContent.toString());
+    }
+
+
+    @Test
+    public void out() {
+        System.out.println("hello");
+        System.out.println("it is I");
+        Assertions.assertEquals("hello\r\nit is I\r\n", outContent.toString());
+    }
+}
--- src/test/java/InsuranceClaimTest.java
+++ src/test/java/InsuranceClaimTest.java
@@ -1,9 +1,26 @@
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Scanner;
 
 public class InsuranceClaimTest {
+    private final java.io.ByteArrayOutputStream outContent = new java.io.ByteArrayOutputStream();
+    private final java.io.ByteArrayOutputStream errContent = new java.io.ByteArrayOutputStream();
+    private final java.io.PrintStream originalOut = System.out;
+    private final java.io.PrintStream originalErr = System.err;
+
+    private void setupTestEnv()
+    {
+        AIUSStarter.init();
+
+        String name = "TestingName";
+        String description = "TestingDescription";
+        String amount = "9999.99";
+
+        Scanner s = new Scanner(String.format("%s\n%s\n%s\n", name, description, amount)).useDelimiter("\n");
+        AIUSStarter.fileInsuranceClaim(s);
+    }
 
     @Test
     public void testFileInsuranceClaim() {
@@ -25,14 +42,7 @@ public class InsuranceClaimTest {
 
     @Test
     public void testInsuranceClaimApproval() {
-        String name = "TestingName";
-        String description = "TestingDescription";
-        String amount = "9999.99";
-
-        AIUSStarter.init();
-        Scanner s = new Scanner(String.format("%s\n%s\n%s\n", name, description, amount)).useDelimiter("\n");
-        AIUSStarter.fileInsuranceClaim(s);
-
+        setupTestEnv();
         InsuranceClaim claim = AIUSStarter.claims.get(0);
 
         String insuranceClaimID = Integer.toString(claim.getID());
